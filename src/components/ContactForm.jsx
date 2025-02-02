a5282ee3683216bf8d440d7c9219ad43fb41876f
Dannnya <komarovskiy.daniil23@gmail.com>
2025-02-02 19:28:53 +0200

    contact

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,12 +1,22 @@
+// import { useState } from 'react';
 import '../scss/ContactForm.scss';
 export const ContactForm = () => {
+  // const [user, setUser] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  }
   return (
-    <form className="contact-form">
-      <input type="text" placeholder="Name" className="form-input" />
-      <input type="email" placeholder="Email" className="form-input" />
-      <input type="text" placeholder="Subject" className="form-input" />
-      <textarea placeholder="Message" className="form-textarea"></textarea>
-          
+    <form className="contact-form" onSubmit={handleSubmit}>
+      <div className="container">
+        <div className="input-row">
+          <input type="text" placeholder="Name" className="form-input" />
+          <input type="email" placeholder="Email" className="form-input" />
+        </div>
+        <input type="text" placeholder="Subject" className="form-input" />
+        <textarea placeholder="Message" className="form-textarea"></textarea>
+      </div>
       <button type="submit" className="form-button">Submit</button>
     </form>
   );
