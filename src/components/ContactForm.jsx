401b2de4d0b7347bd553a65a2d06b8583447194e
Dannnya <komarovskiy.daniil23@gmail.com>
2025-02-05 01:59:49 +0200

    contact

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,23 +1,76 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import '../scss/ContactForm.scss';
 export const ContactForm = () => {
-  // const [user, setUser] = useState('');
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+  };
+  
+  const handleChange = e => {
+    const { name, value } = e.target;
     
+    switch (name) {
+      case 'user':
+        setUser(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'subject':
+        setSubject(value);
+        break;
+      case 'message':
+        setMessage(value);
+        break;
+      default:
+        return;
+    }
   }
+
   return (
-    <form className="contact-form" onSubmit={handleSubmit}>
+    <form className="contact-form" autoComplete="off" onSubmit={ handleSubmit }>
       <div className="container">
         <div className="input-row">
-          <input type="text" placeholder="Name" className="form-input" />
-          <input type="email" placeholder="Email" className="form-input" />
+          <input
+            type="text"
+            placeholder="Name"
+            className="form-input"
+            onChange={handleChange}
+            value={user}
+            name="user" />
+          <input
+            type="email"
+            placeholder="Email"
+            className="form-input"
+            onChange={handleChange}
+            value={email}
+            name="email" />
         </div>
-        <input type="text" placeholder="Subject" className="form-input" />
-        <textarea placeholder="Message" className="form-textarea"></textarea>
+        <input
+          type="text"
+          placeholder="Subject"
+          className="form-input"
+          onChange={handleChange}
+          value={subject}
+          name="subject" />
+        <textarea
+          placeholder="Message"
+          className="form-textarea"
+          onChange={handleChange}
+          value={message}
+          name="message" >
+          </textarea>
       </div>
-      <button type="submit" className="form-button">Submit</button>
+        <button
+          type="submit"
+          className="form-button"
+          >
+          Submit
+        </button>
     </form>
   );
 };
--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,7 +1,45 @@
+import React from 'react';
+import html from '../assets/html.png';
+import css from '../assets/css.png';
+import js from '../assets/js.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import git from '../assets/git.png';
+import sass from '../assets/sass1.png';
+import photo from '../assets/Danyaaa.jpg';
+import '../scss/AboutMePage.scss';
+
 export const AboutMePage = () => {
   return (
-    <div>
-      About Me
+    <div className="container">
+      <div className="title-container">
+        <h1 className="title-about-me">ABOUT ME</h1>
+        <p className="title-text">dgdgdfg
+          dgdgdfgdfg
+        dfgdfgdfg
+          dfgfdfdg
+        fdg</p>
+      </div>
+      <div className="info">
+        <img className="info-photo" src={ photo } alt="" width="300px"/>
+        <div className="info-text">
+          <h1 className="name">Komarovskyi Daniil</h1>
+          <h3>FRONTEND DEVELOPER</h3>
+            <p>text</p>
+        </div>
+      </div>
+      <div className="stack">
+        <h3  className="stack-title">My Stack</h3>
+        <div className="stack-photo">
+          <img src={ html } alt="html" title='HTML'/>
+          <img src={ css } alt="css" title='CSS'/>
+          <img src={ js } alt="js" title='JavaScript'/>
+          <img src={ react } alt="react" title='React' />
+          <img src={ redux } alt="redux" title='Redux Toolkit' />
+          <img src={ git } alt="git" title='Git' />
+          <img src={ sass } alt="sass" width="65px" title='SASS' />
+        </div>
+      </div>
     </div>
   )
 }
