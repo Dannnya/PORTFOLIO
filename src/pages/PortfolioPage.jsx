02da37c127ee2e04fb011d0d2e1eea91e9966375
Dannnya <komarovskiy.daniil23@gmail.com>
2025-02-17 18:29:53 +0200

    scss

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,8 +1,40 @@
+import { useState, useEffect, useRef } from 'react';
+
 export const PortfolioPage = () => {
+  const [time, setTime] = useState(() => new Date());
+  const [count, setCount] = useState(0);
+  const intervalId = useRef(null);
+  const prevCount = useRef(null);
+
+  useEffect( () => {
+    intervalId.current = setInterval(() => {
+      // console.log(`interval every 1s ${Date.now()}`);
+      setTime(new Date());
+    }, 1000)
+  }, [] );
+
+  useEffect(() => { 
+    console.log({ count });
+
+    return () => { 
+      console.clear(prevCount);
+     }
+   }, [ count ] );
+
+  const stop = () => {
+    clearInterval(intervalId.current);
+  };
+
   return (
     <div>
-      Portfolio Page
+      <p>
+        Time: {time.toLocaleTimeString()}
+      </p>
+      <button type="button" onClick={stop}>Click</button>
+
+      <h1>{count}</h1>
+      <button onClick={() => setCount( count + 1 )}> Increment </button>
+      <button onClick={() => setCount( count - 1 )}> Decrement </button>
     </div>
   )
-}
-
+};
