1065a0b4cda70fa0530bad313f7d823d3bceaab0
Dannnya <komarovskiy.daniil23@gmail.com>
2025-02-27 00:09:42 +0200

    portfolio

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,7 +1,27 @@
+import '../scss/PortfolioPage.scss';
+import qwe from "../assets/1.png";
+
 export const PortfolioPage = () => {
   return (
-    <div>
-      Portfolio 
+    <div className="container-portfolio">
+      <div className="title-container">
+        <h1 className="title">
+          PORTFOLIO 
+        </h1>
+      </div>
+      <div className="example-box">
+        <a href="/" target="_blank" >
+        <div className="image">
+          <img className="preview" src={qwe} alt='' width="230px" />  
+          </div>
+        <div className='technologies'>
+          HTML, JavaScript, CSS
+          <p className="description">
+            Dior Landing Page using <br/> metodology of BEM
+          </p>
+        </div>
+        </a>
+      </div>
     </div>
   )
 };
